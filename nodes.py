8ddb5a1b96ebc7d0f7ade2c111a680eaa8cb2991
Josef Vonasek <j.f.vonasek@gmail.com>
2019-04-22 09:08:37 +0200

    generation of node and edge csv

--- nodes.py
+++ nodes.py
@@ -1,6 +1,23 @@
+class Node:
+
+    def __init__(self, id, label, interval):
+        self.id = id
+        self.label = label
+        self.interval = interval
+
+class Edge:
+
+    def __init__(self, id, label, interval, source, target):
+        self.id = id
+        self.label = label
+        self.interval = interval
+        self.source = source
+        self.target = target
+
 class Nodes:
     
     def __init__(self):
+        self.edges = []
         self.nodes = []
         self.steps = []
         self.read('debug.txt')
@@ -23,7 +40,7 @@ class Nodes:
             for i in self.steps[self.cur_index][rnodes]:
                 del self.cur_nodes[i]
             if add == -1: self.cur_index -= 1
-        return (self.steps[self.cur_index]['gonext'], self.cur_nodes)
+        return self.steps[self.cur_index]['gonext'], self.cur_nodes
 
         
     def read(self, fileName):
@@ -43,16 +60,44 @@ class Nodes:
                     if line == '\n': break
                     line = [int(i) for i in line.split()]
                     maxid += 1
-                    self.nodes.append(line)
                     self.steps[-1]['mnodes'].append(maxid)
                     if len(ndids) > line[0]:
                         if ndids[line[0]] != -1: 
                             self.steps[-1]['rnodes'].append(ndids[line[0]])
-                            ndids[line[0]] = -1
+                            for i in range(3):
+                                self.edges[3*ndids[line[0]]+i].interval[1] = len(self.steps)
+                            self.nodes[ndids[line[0]]].interval[1] = len(self.steps)
                         ndids[line[0]] = maxid
                     else:
                         ndids.append(maxid)
+                    for i in range(3):
+                        self.edges.append(Edge(3*maxid+i, i, [len(self.steps), -1], maxid, line[i] if line[i] >= len(ndids) else ndids[line[i]]))
+                    self.nodes.append(Node(maxid, line[-1] >>2, [len(self.steps), -1]))
                 for indx in remove:
                     if ndids[indx] != -1:
                         self.steps[-1]['rnodes'].append(ndids[indx])
                         ndids[indx] = -1
+                        for i in range(3):
+                            self.edges[3*ndids[indx]+i].interval[1] = len(self.steps)
+                        self.nodes[ndids[indx]].interval[1] = len(self.steps)
+
+        for n in self.nodes:
+            if n.interval[1] == -1:
+                n.interval[1] = len(self.steps)
+
+        for e in self.edges:
+            if e.interval[1] == -1:
+                e.interval[1] = len(self.steps)
+
+    def write(self):
+        nodes = open('debug_nodes.csv', mode='w')
+        print('id, label, start, end', file=nodes)
+        for n in self.nodes:
+            print(f'{n.id}, {n.label}, {n.interval[0]}, {n.interval[1]}', file=nodes)
+        edges = open('debug_edges.csv', mode='w')
+        print('id, label, start, end, source, target', file=edges)
+        for e in self.edges:
+            print(f'{e.id}, {e.label}, {n.interval[0]}, {n.interval[1]}, {e.source}, {e.target}', file=edges)
+
+if __name__ == '__main__':
+    Nodes().write()
