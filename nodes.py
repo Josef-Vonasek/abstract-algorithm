c3b0490448c7259638429f72502b45f3752f1ae3
Josef Vonasek <j.f.vonasek@gmail.com>
2019-04-28 10:04:04 +0200

    fixed few bugs

--- nodes.py
+++ nodes.py
@@ -71,8 +71,11 @@ class Nodes:
                     else:
                         ndids.append(maxid)
                     for i in range(3):
-                        self.edges.append(Edge(3*maxid+i, i, [len(self.steps), -1], maxid, line[i] if line[i] >= len(ndids) else ndids[line[i]]))
-                    self.nodes.append(Node(maxid, line[-1] >>2, [len(self.steps), -1]))
+                        self.edges.append(Edge(3*maxid+i, i, [len(self.steps), -1], maxid, line[i+1]>>2))
+                    self.nodes.append(Node(maxid, line[-1]>>2, [len(self.steps), -1]))
+                for n in self.steps[-1]['mnodes']:
+                    for i in range(3):
+                        self.edges[3*n+i].target = ndids[self.edges[3*n+i].target]
                 for indx in remove:
                     if ndids[indx] != -1:
                         self.steps[-1]['rnodes'].append(ndids[indx])
@@ -88,22 +91,37 @@ class Nodes:
         for e in self.edges:
             if e.interval[1] == -1:
                 e.interval[1] = len(self.steps)
+      
+        self.steps[0]['mnodes'] = [n for n in self.steps[0]['mnodes'] if n not in self.steps[0]['rnodes']]
+        self.steps[0]['rnodes'] = []
 
     def write(self):
-        nodes = open('debug_nodes.csv', mode='w')
-        print('iteration, action, id, label', file=nodes)
-        edges = open('debug_edges.csv', mode='w')
-        print('iteration, action, id, label, source, target', file=edges)
+        nodes = open('nodes.csv', mode='w')
+        print('action, id, label', file=nodes)
+        edges = open('edges.csv', mode='w')
+        print('action, id, source, target, label', file=edges)
         for i, step in enumerate(self.steps):
+            print(f'NEW ITERATION, {i},', file=nodes)
+            print(f'NEW ITERATION, {i}, , ,', file=edges)
             for n in step['rnodes']:
-                print(f'{i}, remove, {n},',  file=nodes)
+                print(f'remove, {n},',  file=nodes)
                 for e in range(3*n,3*n+3):
-                    print(f'{i}, remove, {e}, , ,', file=edges)
+                    t = self.edges[e].target
+                    if n < t:
+                        print(f'remove, {e}, , ,', file=edges)
+                    elif t not in step['rnodes']:
+                        for i in range(3):
+                            if self.edges[3*t+i].target == n:
+                                print(f'remove, {3*t+i}, , ,', file=edges)
+                                break
             for n in step['mnodes']:
-                print(f'{i}, insert, {n}, {self.nodes[n].label}',  file=nodes)
+                print(f'insert, {n}, {self.nodes[n].label}',  file=nodes)
                 for e in range(3*n,3*n+3):
                     edge = self.edges[e]
-                    print(f'{i}, insert, {e}, {edge.label}, {edge.source}, {edge.target}', file=edges)
+                    if edge.source < edge.target:
+                       print(f'insert, {e}, {edge.source}, {edge.target}, {edge.label}', file=edges)
+                    elif step['mnodes'][0] > edge.target:
+                       print(f'insert, {e}, {edge.target}, {edge.source}, {edge.label}', file=edges)
 
 if __name__ == '__main__':
     Nodes().write()
